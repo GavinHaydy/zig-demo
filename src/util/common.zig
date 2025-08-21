c68ead535a08af47d5e50929fa68d5ce38865ac5
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-08-22 00:07:37 +0800

    feat: struct to json

--- src/util/common.zig
+++ src/util/common.zig
@@ -20,4 +20,44 @@ pub fn containsEnum(comptime T: type, haystack: []const T, needle: T) bool {
     return false;
 }
 
-// todo 获取 json 值
+// 转json
+pub fn fromJson(comptime T: type, value: std.json.Value) !T {
+    const info = @typeInfo(T);
+    if (info != .@"struct") @compileError("fromJson only works on structs");
+
+    var result: T = undefined;
+
+    inline for (info.@"struct".fields) |field| {
+        const ftype = field.type;
+        const fname = field.name;
+
+        const jsonVal = value.object.get(fname) orelse
+            return error.MissingField;
+
+        switch (@typeInfo(ftype)) {
+            .int => {
+                @field(result, fname) = @intCast(jsonVal.integer);
+            },
+            .float => {
+                @field(result, fname) = @floatCast(jsonVal.float);
+            },
+            .bool => {
+                @field(result, fname) = jsonVal.bool;
+            },
+            .pointer => {
+                // ✅ 特殊处理 string (即 []const u8)
+                if (ftype == []const u8) {
+                    @field(result, fname) = jsonVal.string;
+                } else {
+                    @compileError("Unsupported pointer type: " ++ @typeName(ftype));
+                }
+            },
+            .array => {
+                @compileError("Array fields not supported yet: " ++ @typeName(ftype));
+            },
+            else => @compileError("Unsupported type in fromJson: " ++ @typeName(ftype)),
+        }
+    }
+
+    return result;
+}
