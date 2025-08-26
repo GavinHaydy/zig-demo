be1505ac3290228ea46de4ab6185ed07e36ffda1
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-08-27 00:55:08 +0800

    feat: jwt interceptor init

---
+++ src/middlewares/jwtInterceptor.zig
@@ -0,0 +1,32 @@
+const zap = @import("zap");
+const std = @import("std");
+const jwt = @import("../util/jwt.zig");
+
+
+pub fn jwtInterceptor(r: zap.Request) bool {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    std.debug.print("{}", .{r});
+
+    const token =r.getHeader("Authorization") orelse "";
+    if (std.mem.eql(u8, token, "")) {
+        const bef_r = .{
+            .code=200,
+            .status=false,
+            .msg = "token 没传",
+        };
+
+        const result = std.json.stringifyAlloc(allocator, bef_r, .{}) catch {
+            r.sendJson("{\"code\":500,\"msg\":\"oom\"}") catch {};
+            return false;
+        };
+        defer allocator.free(result);
+
+        _ = r.sendJson(result) catch {};
+        return false;
+    }
+
+    return true;
+    // return try jwt.verifyJwt(token,"test");
+}
