1caa29869f9d8037f13526f1582143d8dcebbf7e
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-08-30 20:35:09 +0800

    chore: add verify userId

--- src/middlewares/jwtInterceptor.zig
+++ src/middlewares/jwtInterceptor.zig
@@ -1,6 +1,7 @@
 const zap = @import("zap");
 const std = @import("std");
 const jwt = @import("../util/jwt.zig");
+const Enums = @import("../enums/http.zig");
 
 
 pub fn jwtInterceptor(r: zap.Request) bool {
@@ -12,13 +13,13 @@ pub fn jwtInterceptor(r: zap.Request) bool {
     const token =r.getHeader("authorization") orelse "";
     if (std.mem.eql(u8, token, "")) {
         const bef_r = .{
-            .code=200,
-            .status=false,
-            .msg = "token 没传",
+            .code= @intFromEnum(Enums.StatusCode.unauthorized),
+            .msg = Enums.StatusCode.unauthorized.toString(),
+            .data = {}
         };
 
         const result = std.json.stringifyAlloc(allocator, bef_r, .{}) catch {
-            r.sendJson("{\"code\":500,\"msg\":\"oom\"}") catch {};
+            r.sendJson("{\"code\":401,\"msg\":\"Unauthorized\"}") catch {};
             return false;
         };
         defer allocator.free(result);
@@ -26,6 +27,25 @@ pub fn jwtInterceptor(r: zap.Request) bool {
         _ = r.sendJson(result) catch {};
         return false;
     }
+
+    const userId =try jwt.parseToken(token);
+    if (std.mem.eql(u8, userId, "")) {
+        const rsp = .{
+          .code = @intFromEnum(Enums.StatusCode.unauthorized),
+            .msg = Enums.StatusCode.unauthorized.toString(),
+            .data = {},
+        };
+
+        const result = std.json.stringifyAlloc(allocator, rsp, .{}) catch {
+            r.sendJson("{\"code\":401,\"msg\":\"Unauthorized\"}") catch {};
+            return false;
+        };
+        defer allocator.free(result);
+
+        _ = r.sendJson(result) catch {};
+        return false;
+    }
+
 
     return true;
     // return try jwt.verifyJwt(token,"test");
